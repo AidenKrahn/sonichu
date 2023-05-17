876ad4a44b26b6996d8a768126fdfb3599a2fff6
AidenKrahn <125599022+AidenKrahn@users.noreply.github.com>
2023-05-16 21:03:04 -0600

    Add files via upload

---
+++ sonichu.py
@@ -0,0 +1,19 @@
+#Sonichu
+#AidenKrahn
+#Started May 16
+
+import pygame, random
+
+pygame.init()
+
+window_width = 800
+window_height = 600
+win = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Sonichu: CWC's Love Quest")
+clock = pygame.time.clock
+
+
+
+class Player(object):
+    def __init__(self,x,y,width,height)
+    pass
