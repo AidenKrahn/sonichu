0e4fa8ec470e751173947ef999bf341ede56ae4a
AidenKrahn <125599022+AidenKrahn@users.noreply.github.com>
2023-05-19 12:50:47 -0600

    Update sonichu.py

--- sonichu.py
+++ sonichu.py
@@ -2,7 +2,7 @@
 #AidenKrahn
 #Started May 16
 
-import pygame, random, math
+import pygame, random
 
 pygame.init()
 
@@ -10,8 +10,13 @@ window_width = 800
 window_height = 600
 win = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Sonichu: CWC's Love Quest")
+
 clock = pygame.time.Clock()
-ts = pygame.image.load('titscr.png')
+
+ts = pygame.image.load('titscr.png')#titlescreen
+
+
+#define rgb colors
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
@@ -21,29 +26,41 @@ yellow = (255,255,0)
 orange = (255,128,0)
 purple = (153,0,153)
 
-
-
+#----classes----
+#what human controls
 class Player(object):
-    def __init__(self,x,y,width,height):
+    def __init__(self,x,y,width,height,fr):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.hitbox = []
-        self.stam = 10
-        self.vel = 10
-        self.acc = 0
-        self.jump = 10
-        self.isJump = 0
-        self.facing = 1
+        self.stam = 10#stamina bar
+        self.vel = 10#
+        self.acc = 0#speed up and slow down gradually instead of 0 - 100
+        self.jump = 10#how high can he jump
+        self.isJump = 0#whether he is jumping
+        self.facing = 1#what way is he facing
+        self.fr = fr
         
     def jump(self,win,x,y,width,height):
         pass
     
     def punch(self,win,hitbox):
         pass
-        
     
+    def run(self,win):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.facing = 1
+            self.x += self.vel * self.facing
+            
+        elif keys[pygame.K_LEFT]:
+            self.facing = -1
+            self.x += self.vel * self.facing
+                
+        
+#normal enemy
 class Troll(object):
     def __init__(self,x,y,width,height):
         self.x = x
@@ -51,34 +68,44 @@ class Troll(object):
         self.width = width
         self.height = height
         self.vel = 5
-    
-bg = pygame.transform.scale(ts, (800,600))
+
+ts = pygame.transform.scale(ts, (800,600))
 run = True
 Srun = pygame.image.load('Sr.png').convert_alpha()
 
-def get_image(sheet,frame,width,height, color, big):
+
+
+def get_image(sheet,frame,width,height, color, big):#taking an image from a sprite sheet
     image = pygame.Surface((width,height)).convert_alpha()
-    image.blit(sheet, (0,0), (0,(frame * height), (width * frame) + width,(height * frame) + width))
+    image.blit(sheet, (0,0), (0,(frame * height), 22,(height * frame) + width))
     image = pygame.transform.scale(image,(big,big))
     image.set_colorkey(color)
     return image
 
-frame0 = get_image(Srun,0,22,22, black, 100)
+def redraw():
+    pass
 
-while run == True:
-    clock.tick(50)
-    win.blit(bg,(0,0))
-    
-    win.blit(frame0, (250,250))
-    
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            quit()
-            
+
+def titlescreen(run,bg,f):
+    while run == True:
+        clock.tick(50)
+        pygame.time.delay(30)
+        win.blit(bg,(0,0))
+        win.blit(f,(s.x,s.y))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                quit()
+        s.run(win)
+                
         pygame.display.update()
+                
             
-        keys = pygame.key.get_pressed()
+            
+f0 = get_image(Srun,0,22,22, black,100)
+s = Player(250,250,100,100,f0)
+            
+titlescreen(run,ts,s.fr)
     
 pygame.quit()
